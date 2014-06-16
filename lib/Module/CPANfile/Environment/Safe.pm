d08cad78b9a0131d5c53f0d1a71682fb7c68c3ae
mephinet <mephinet@gmx.net>
2014-06-16 08:05:02 +0200

    Matching all lines of safe.t

--- lib/Module/CPANfile/Environment/Safe.pm
+++ lib/Module/CPANfile/Environment/Safe.pm
@@ -5,21 +5,24 @@ use Carp;
 use warnings;
 use strict;
 
+#use re 'debug';
+
 my $module_re = qr/['"] [A-Za-z:]+ ['"]/x;
 my $version_re = qr/v?[0-9]+\.[0-9_.]+/x;
 my $operator_re = qr/(?: < | <= | > | >= | == | != )/x;
-my $version_requirement_re = qr/['"]? $operator_re? $version_re ['"]?/x;
-my $multiple_version_requirement_re = qr/ $version_requirement_re (?: \s* , \s* $version_requirement_re )* /x;
-my $requirement_re = qr/(?: requires|recommends|suggests|conflicts) \s+ $module_re (?: \s*,\s* $multiple_version_requirement_re)? /x;
+my $version_requirement_re = qr/$operator_re? \s* $version_re/x;
+my $multiple_version_requirement_re = qr/['"]? \s* $version_requirement_re (?: \s* , \s* $version_requirement_re )*  \s* ['"]?/x;
+my $module_version_requirement_re = qr/$module_re (?: \s*,\s* $multiple_version_requirement_re)?/x;
+my $requirement_re = qr/\s* (?: requires|recommends|suggests|conflicts) \s+ $module_version_requirement_re  \s*; \s* $/mx;
 
 my $phase_re = qr/['"] (?: configure | build | test | runtime | develop ) ['"] /x;
-my $on_re = qr/on \s+ $phase_re \s* => \s* sub \s* { $requirement_re* } \s* /x;
+my $on_re = qr/\s* on \s+ $phase_re \s* => \s* sub \s* { \s* $requirement_re* \s* }  \s*; \s* $/mx;
 
 my $feature_re = qr/xxxxxxxxxxxxx/x; ### XXX
 
-my $phase_requires_re = qr/ (?: configure | build | test | author )_requires \s+ $module_re (?: \s*,\s* $multiple_version_requirement_re)?/x;
+my $phase_requires_re = qr/\s* (?: configure | build | test | author )_requires \s+ $module_version_requirement_re  \s*; \s* $/mx;
 
-my $statement_re = qr/^ \s* ($requirement_re|$on_re|$feature_re|phase_requires_re) \s*; \s* $/mx;
+my $statement_re = qr/^ \s* ($requirement_re|$on_re|$feature_re|$phase_requires_re)/mx;
 
 sub parse {
     my ($self, $code) =  @_;
